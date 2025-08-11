71e4be53a19071334a4790e9e8195b47ff99fc74
hajunk05 <hajunk05@gmail.com>
2025-08-11 23:50:23 +1200

    customised the navigation bar with logos and css

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,41 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { LinkContainer } from 'react-router-bootstrap'
+import logo from './images/keb_logo.png'
+import './App.css'
 
 function App() {
 	return (
 		<div>
-			<Navbar
-				bg="primary"
-				data-bs-theme="dark"
-			>
+			<Navbar className="custom-navbar">
 				<Container>
 					<LinkContainer to="/">
-						<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+						<Navbar.Brand>
+							<img
+								src={logo}
+								height="30"
+								className="d-inline-block align-top navbar-logo me-5"
+								alt="KEB logo"
+							/>{' '}
+						</Navbar.Brand>
 					</LinkContainer>
 					<Nav className="me-auto">
-						<LinkContainer to="/">
+						<LinkContainer
+							to="/"
+							className="px-3"
+						>
 							<Nav.Link> Home </Nav.Link>
 						</LinkContainer>
-						<LinkContainer to="/events">
+						<LinkContainer
+							to="/events"
+							className="px-3"
+						>
 							<Nav.Link> Events </Nav.Link>
 						</LinkContainer>
-						<LinkContainer to="/about">
+						<LinkContainer
+							to="/about"
+							className="px-3"
+						>
 							<Nav.Link> About </Nav.Link>
 						</LinkContainer>
 					</Nav>
